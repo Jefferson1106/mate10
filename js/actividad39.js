25a469d62337db56fe9465f4b4cdc2c353c9143f
Jefferson <jeffersonjitala11@gmail.com>
2018-08-16 17:41:05 -0500

    jueves

--- js/actividad39.js
+++ js/actividad39.js
@@ -3,17 +3,17 @@ numero_pagina(29);
 
 var act13 = [
     ['<table ><tr><td><img src="img/i3_p29_act5.jpg"></td><td>'+
-    '<input type="text"   size="1" style="width:30px" class="form-control border-blue respuestas">'+
+    '<input type="text"   size="1" style="width:50px" class="form-control border-blue respuestas">'+
     '<input type="hidden" style="color:blue" class="solucion" value="2"></td>'+
     '</tr></table>'
     ],
     ['<table ><tr><td><img src="img/i1_p29_act5.jpg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>'+
-    '<input type="text"   size="1" style="width:30px" class="form-control border-blue respuestas">'+
+    '<input type="text"   size="1" style="width:50px" class="form-control border-blue respuestas">'+
     '<input type="hidden" style="color:blue" class="solucion" value="3"></td>'+
     '</tr></table>'
     ],
     ['<table ><tr><td><img src="img/i2_p29_act5.jpg"></td><td>'+
-    '<input type="text"   size="1" style="width:30px" class="form-control border-blue respuestas">'+
+    '<input type="text"   size="1" style="width:50px" class="form-control border-blue respuestas">'+
     '<input type="hidden" style="color:blue" class="solucion" value="20"></td>'+
     '</tr></table>'
     ]
